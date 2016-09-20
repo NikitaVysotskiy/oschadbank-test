f5364138cdbcf9d3b868354566aa0346d8973df9
Nikita <fordf150svtraptor@gmail.com>
2016-09-20 19:50:22 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    /*$("li.top>a").on("mouseover", function () {
+        $("li.top>a::before").css({
+            width: "100%",
+            transition: "width 0.3s 0s"
+        });
+
+    });*/
+    
+    /*$("#sidebar-wrapper").mouseover( function () {
+        $("#sidebar-wrapper").css({width :"240px"});
+    });*/
+});
